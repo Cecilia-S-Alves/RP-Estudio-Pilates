917fbbcbef9a7abe90f0f3830e33df5485d1f4bb
Cecilia-S-Alves <ailicecavlisad@gmail.com>
2025-09-17 17:28:55 -0300

    mostrar dados do banco na tela

--- pilatesStudio/screens/Alunos.js
+++ pilatesStudio/screens/Alunos.js
@@ -1,8 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 import { db } from '../ControleFirebase';
 import { collection,doc,getDocs } from "firebase/firestore";
 import { useEffect, useState } from 'react';
-import { FlatList } from 'react-native-web';
 
 export default function Alunos() {
   const [Alunos,setAlunos] = useState([]);
@@ -26,11 +25,36 @@ export default function Alunos() {
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Alunos</Text>
-       <Text style={styles.texto1}>mostrar dados na tela</Text> 
-    </View>);
-
+       <FlatList data={Alunos} renderItem={({item}) => (
+          <View style={styles.background}>
+            <TouchableOpacity style={styles.touchContainer}>  
+                <View style={styles.card}>
+                  <Text style={styles.texto1}>{item.Nome}</Text>
+                  <View style={styles.nomedaview}>
+                    <View style={{flexDirection: 'row'}}>
+                      <Text style={[styles.texto2, { textDecorationLine: 'underline' }]}>Idade</Text>
+                      <Text style={styles.texto2}>: {item.Idade}</Text>
+                    </View>
+                    <View style={{flexDirection: 'row'}}>
+                      <Text style={[styles.texto2, { textDecorationLine: 'underline' }]}>Sexo</Text>
+                      <Text style={styles.texto2}>: {item.Sexo}</Text>
+                    </View>
+                  </View>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text style={[styles.texto2, { textDecorationLine: 'underline' }]}>Patologia</Text>
+                    <Text style={styles.texto2}>: {item.Patologia}</Text>
+                  </View>
+                    <View style={{flexDirection: 'row'}}>
+                    <Text style={[styles.texto2, { textDecorationLine: 'underline' }]}>Turma</Text>
+                    <Text style={styles.texto2}>: {item.Turma}</Text>
+                  </View>
+                </View>
+            </TouchableOpacity>
+          </View>
+        )} keyExtractor={item => item.id} showsVerticalScrollIndicator={false} numColumns={1} />
+    </View>
     
-
+  );
 
 }
 
@@ -50,6 +74,37 @@ const styles = StyleSheet.create({
   texto1: {
     fontFamily:'Overlock SC',
     color:'#4e5a5e',
-    fontSize: 40
+    fontSize: 38,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+    texto2: {
+    fontFamily:'Overlock SC',
+    color:'#4e5a5e',
+    fontSize: 32,
+
+  },
+  background: {
+    alignSelf:'center',
+    width: 380,
+    height: 200,
+    backgroundColor: '#b99470',
+    borderRadius: 15,
+    marginBottom: 20,
+    
+  },
+  card:{
+    backgroundColor: '',
+    flex: 1,
+    alignItems: 'center',
+    
+  },
+  nomedaview:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  touchContainer:{
+    flex:1,
   }
 });
